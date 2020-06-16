99edd867a329f389461b64b79516f880c22113d6
WU-GUAN-YI <ian0989082313@gmail.com>
2020-06-16 19:35:42 +0800

    w1

---
+++ blogen_app/routes/blogen.js
@@ -0,0 +1,1 @@
+var express = require("express");var router = express.Router();router.get("/", function(req, res, next) {  res.render("dashboard", { title: "Blogen Dashoard" });});module.exports = router;
